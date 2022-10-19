3400e35f47bb930548306e7b699835c07ac93ed2
JustLittleFive <newyue2010@163.com>
2022-10-19 10:38:01 +0800

    new

--- lab5/FileTypeParser.java
+++ lab5/FileTypeParser.java
@@ -14,9 +14,8 @@
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-package lab5;
 
-import java.io.*;
+ import java.io.*;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 
